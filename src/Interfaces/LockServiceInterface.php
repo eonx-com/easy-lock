4dc8029f88dfffffe6a7592979d9a9423a3fb753
natepage <nathan.page86@gmail.com>
2021-03-17 11:27:20 +1100

    [Monorepo] use grep to remove config header

---
+++ src/Interfaces/LockServiceInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EonX\EasyLock\Interfaces;
+
+use Closure;
+use Symfony\Component\Lock\LockInterface;
+
+interface LockServiceInterface
+{
+    public function createLock(string $resource, ?float $ttl = null): LockInterface;
+
+    /**
+     * @return null|mixed
+     */
+    public function processWithLock(LockDataInterface $lockData, Closure $func);
+}
