3ed3fb0e00ec6fa474ce003337d4116f59140c9d
Aleksey Polyvanyi <alexndlm@gmail.com>
2024-07-01 11:16:38 +0200

    [Monorepo] Rename HttpKernel to Kernel in tests (#1452)

--- tests/Stub/Kernel/KernelStub.php
+++ tests/Stub/Kernel/KernelStub.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace EonX\EasyLock\Tests\Stub\HttpKernel;
+namespace EonX\EasyLock\Tests\Stub\Kernel;
 
 use EonX\EasyLock\Bundle\EasyLockBundle;
 use Psr\Log\LoggerInterface;
